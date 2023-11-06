8b1507b8abb5dc550d85df18b854172e8a3f9c71
uladzimir <01myit@gmail.com>
2023-11-06 15:43:00 +0300

    Added toggle function for the counter.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,23 +1,31 @@
 import classes from "./Counter.module.css" 
 import { useSelector, useDispatch } from 'react-redux'
 const Counter = () => {
-  const toggleCounterHandler = () => {} 
-  const counter = useSelector(state => state.counter)
+  const stateObject = useSelector(state => { return { counter: state.counter, isVisibleCounter: state.isVisibleCounter}})
   const dispatch = useDispatch()
   function changeCounter(action, payload) {
     dispatch({ type: action, payload: payload})
   }
+  const toggleCounterHandler = (action, payload) => {
+    dispatch({ type: action, payload: payload })
+  } 
   return (
     <main className={classes.counter}>
       <h1>Счётчик</h1>
-      <div className={classes.value}>{counter}</div>
-      <div>
-        <button onClick={() => changeCounter('CHANGE_VALUE', 1)}>+1</button>
-        <button onClick={() => changeCounter('CHANGE_VALUE', 7)}>+7</button>
-        <button onClick={() => changeCounter('CHANGE_VALUE', -1)}>-1</button>
-        <button onClick={() => changeCounter('CHANGE_VALUE', -7)}>-7</button>
-      </div>
-      <button onClick={toggleCounterHandler}>Спрятать / Показать</button>
+      {
+        stateObject.isVisibleCounter &&
+        <div className={classes.value}>{stateObject.counter}</div>
+      }
+      {
+        stateObject.isVisibleCounter &&
+        <div>
+          <button onClick={() => changeCounter('CHANGE_VALUE', 1)}>+1</button>
+          <button onClick={() => changeCounter('CHANGE_VALUE', 7)}>+7</button>
+          <button onClick={() => changeCounter('CHANGE_VALUE', -1)}>-1</button>
+          <button onClick={() => changeCounter('CHANGE_VALUE', -7)}>-7</button>
+        </div>
+      }
+      <button onClick={() => toggleCounterHandler('TOGGLE', !stateObject.isVisibleCounter)}>Спрятать / Показать</button>
     </main>
   ) 
 } 
--- src/store/redux.js
+++ src/store/redux.js
@@ -1,9 +1,15 @@
 import { legacy_createStore } from 'redux'
-function reducer(state = { counter: 0 }, action) {
+function reducer(state = { counter: 0, isVisibleCounter: false }, action) {
   switch (action.type) {
     case 'CHANGE_VALUE':
       return {
-        counter: state.counter + action.payload
+        counter: state.counter + action.payload,
+        isVisibleCounter: true
+      }
+    case 'TOGGLE': 
+      return {
+        counter: state.counter,
+        isVisibleCounter: state.isVisibleCounter = action.payload
       }
     default: return state
   }
